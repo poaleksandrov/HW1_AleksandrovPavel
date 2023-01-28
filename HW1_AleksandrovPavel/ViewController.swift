f374485f78d7a82154f228c2ca738ca1795a0968
pavelalexandrov <poaleksandrov@gmail.com>
2023-01-28 17:09:31 +0300

    Initial Commit

---
+++ HW1_AleksandrovPavel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW1_AleksandrovPavel
+//
+//  Created by Павел Александров on 28.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
